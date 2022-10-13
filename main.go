e87cbe9701c0089ac70df0d75fbc260d7ba7a8da
gammazero <gammazero@users.noreply.github.com>
2022-10-12 19:41:50 -0700

    Rebase and add periodic file check

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	cli "github.com/urfave/cli/v2"
 )
+
+// configCheckInterval determines how frequently the config file is checked for
+// changes, to see if it needs to be reloaded. Set this to 0 to disable
+// checking the config file.
+const configCheckInterval = 5 * time.Second
 
 func main() {
 	app := &cli.App{
@@ -48,24 +54,70 @@ func main() {
 				return err
 			}
 
-			reload := make(chan os.Signal, 1)
-			signal.Notify(reload, syscall.SIGHUP)
+			sighup := make(chan os.Signal, 1)
+			signal.Notify(sighup, syscall.SIGHUP)
 
 			done := s.Serve()
 
+			var (
+				cfgPath  string
+				modTime  time.Time
+				ticker   *time.Ticker
+				timeChan <-chan time.Time
+			)
+			if configCheckInterval != 0 {
+				modTime, _, err = fileChanged(cfgPath, modTime)
+				if err != nil {
+					log.Error(err)
+				}
+				ticker = time.NewTicker(configCheckInterval)
+				timeChan = ticker.C
+
+				cfgPath := s.cfgBase
+				if cfgPath == "" {
+					cfgPath, err = Filename("")
+					if err != nil {
+						return err
+					}
+				}
+			}
+
+			reloadSig := make(chan struct{}, 1)
 			for {
 				select {
+				case <-sighup:
+					select {
+					case reloadSig <- struct{}{}:
+					default:
+					}
 				case <-exit:
 					return nil
 				case err := <-done:
 					return err
-				case <-reload:
+				case <-reloadSig:
 					err := s.Reload()
 					if err != nil {
 						log.Warnf("couldn't reload servers: %s", err)
 					}
+				case <-timeChan:
+					var changed bool
+					modTime, changed, err = fileChanged(s.cfgBase, modTime)
+					if err != nil {
+						log.Errorw("Cannot stat config file", "err", err, "path", cfgPath)
+						ticker.Stop()
+						ticker = nil
+						timeChan = nil // reading from nil channel blocks forever
+						continue
+					}
+					if changed {
+						reloadSig <- struct{}{}
+					}
 				}
 			}
+			if ticker != nil {
+				ticker.Stop()
+			}
+			return nil
 		},
 	}
 	err := app.Run(os.Args)
@@ -74,4 +126,15 @@ func main() {
 		os.Exit(1)
 	}
 	os.Exit(0)
+}
+
+func fileChanged(filePath string, modTime time.Time) (time.Time, bool, error) {
+	fi, err := os.Stat(filePath)
+	if err != nil {
+		return modTime, false, err
+	}
+	if fi.ModTime() != modTime {
+		return fi.ModTime(), true, nil
+	}
+	return modTime, false, nil
 }
