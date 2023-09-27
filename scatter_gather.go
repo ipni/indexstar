849f8833f6211b4b4558ced86924ba5190b01a6a
Andrew Gillis <gammazero@users.noreply.github.com>
2023-09-27 15:38:48 -0700

    Reduce log noise (#175)
    * Log DeadlineExceeded messages at debug
    
    Similar to context canceled messages there are filling up the logs.
    
    Fixes #173

--- scatter_gather.go
+++ scatter_gather.go
@@ -45,7 +45,7 @@ func (sg *scatterGather[B, R]) scatter(ctx context.Context, forEach func(context
 				if errors.Is(err, context.Canceled) {
 					log.Debugw("Scatter on target canceled", "target", target.URL().Host)
 				} else if errors.Is(err, context.DeadlineExceeded) {
-					log.Infow("failed to scatter on target because context deadline exceeded", "target", target.URL().Host, "maxWait", sg.maxWait)
+					log.Debugw("failed to scatter on target because context deadline exceeded", "target", target.URL().Host, "maxWait", sg.maxWait)
 				} else {
 					log.Errorw("failed to scatter on target", "target", target.URL().Host, "err", err, "maxWait", sg.maxWait)
 				}
